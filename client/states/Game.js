96f051e418d75e7bd13f44268577e9462eb81b26
Jason Tham <jtham@sbcglobal.net>
2017-05-20 00:20:32 -0500

    added positions to database, create sprites from database

--- client/states/Game.js
+++ client/states/Game.js
@@ -5,13 +5,23 @@ const Gamestate = {
         this.scale.pageAlignVerically = true;
     },
     preload() {
-        this.load.image('backyard', 'images/backyard.png');
+        this.load.image('backyard', 'images/backyard.png'); 
         this.load.image('candy', 'images/candy.png');
         this.load.image('apple', 'images/apple.png');
         this.load.image('duck', 'images/duck.png');
     },
     create() {
         this.background = this.add.sprite(0, 0, 'backyard');
+        this.items = this.add.group();
+
+        let positions;
+        Tracker.autorun(() => {
+            positions = Positions.find();
+            console.log(`number of items: ${positions.count()}`);
+            positions.forEach((pos) => {
+                this.items.create(pos.x, pos.y, pos.asset);
+            });
+        });
     }
 };
 
